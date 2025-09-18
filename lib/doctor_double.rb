d10fcea384ee88779ffec13c8f426d4a4358e330
3olkin <an.zolkin@outlook.com>
2025-09-18 14:40:01 +0200

    project initialization

---
+++ lib/doctor_double.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "doctor_double/version"
+
+module DoctorDouble
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/doctor_double/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module DoctorDouble
+  VERSION = "0.1.0"
+end
